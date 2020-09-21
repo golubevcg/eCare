b9792bdb142c5d34d3d2888c6cecc4dff34d12e7
Andrew Golubev <golubevcg@gmail.com>
2020-09-21 13:30:58 +0300

    wrk

--- src/main/java/eCare/controllers/MainPageController.java
+++ src/main/java/eCare/controllers/MainPageController.java
@@ -1,23 +1,17 @@
 package eCare.controllers;
 
-import eCare.model.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.sql.SQLOutput;
 
 @Controller
 public class MainPageController {
 
-//    @GetMapping("/")
-//    public String getMain(Model model, CsrfToken token){
-//        return "main";
-//    }
+    @GetMapping("/")
+    public String getMain(Model model, CsrfToken token){
+        return "main";
+    }
 
     @GetMapping("/clientOffice")
     public String getClientOffice(Model model, CsrfToken token){
@@ -44,11 +38,4 @@ public class MainPageController {
         return "newOption";
     }
 
-    //THIS PAGE FOR TESTING PURPOSES ONLY
-    @GetMapping("/")
-    public String getReg(Model model, CsrfToken token){
-        model.addAttribute("userForm", new User());
-        return "registration";
-    }
-
-}
+   }
